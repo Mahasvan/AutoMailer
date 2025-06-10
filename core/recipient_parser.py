baab59446f5098f12778145ce9e509c97f37e082
Nilaa A J <nilaa2310421@ssn.edu.in>
2025-06-10 21:24:57 +0530

    feat(parser): add parsing logic for recipients and email template

---
+++ core/recipient_parser.py
@@ -0,0 +1,22 @@
+import csv
+import re
+
+def load_recipients(f_path):
+    recipients = []
+    with open(f_path, newline = '', encoding='utf-8-sig') as f:
+        reader = csv.reader(f)
+        for line in reader:
+            if len(line) < 2:
+                continue
+
+            email = line[1]
+            valid = re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email)
+            if not valid:
+                continue
+
+            recipients.append(line)
+    print(recipients)
+
+
+if __name__ == '__main__':
+    load_recipients(r'data\input.csv')
